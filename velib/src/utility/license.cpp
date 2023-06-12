f21f6049e93523b693e0c609f05e61e45cad87ac
raoufriahi <riahi.raouf@gmail.com>
2023-06-12 21:31:40 +0200

    documentation for license

--- velib/src/utility/license.cpp
+++ velib/src/utility/license.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018 Heimdall
+ * Copyright (c) 2017-2013 Heimdall
  *
  * The computer program contained herein contains proprietary
  * information which is the property of Heimdall.
@@ -10,20 +10,49 @@
  */
 #include "license.hpp"
 
+/**
+ * Sets the license.
+ *
+ * @param lic The license to be set.
+ * @return True if the license was set successfully, false otherwise.
+ */
 BOOL SetIc(string lic)
 {
 	return TRUE;
 }
+
+/**
+ * Checks if the license is valid.
+ *
+ * @return True if the license is valid, false otherwise.
+ */
 BOOL IsIced()
 {
 	return TRUE;
 }
+/**
+ * Sets the license using the provided license string.
+ *
+ * @param strLicense A reference to the license string.
+ * @return True if the license was set successfully, false otherwise.
+ */
 BOOL LicSetLicense(astring &strLicense)
 {
 	return TRUE;
 }
-BOOL LicGetLicenseInfo(astring &strHostId, int &ch, astring &type,
-	astring &startTime, astring &expireTime)
+
+/**
+ * Retrieves information about the license.
+ *
+ * @param strHostId A reference to the host ID.
+ * @param ch A reference to the license channel.
+ * @param type A reference to the license type.
+ * @param startTime A reference to the license start time.
+ * @param expireTime A reference to the license expiration time.
+ * @return True if the license information was retrieved successfully, false otherwise.
+ */
+BOOL LicGetLicenseInfo(astring &strHostId, int &ch, astring &type, 
+                      astring &startTime, astring &expireTime)
 {
 	strHostId = "HEIMDALL::COMMUNITY";
 	ch = 512;
@@ -32,18 +61,39 @@ BOOL LicGetLicenseInfo(astring &strHostId, int &ch, astring &type,
 	expireTime = std::to_string((s64)(time(NULL)));
 	return TRUE;
 }
+
+/**
+ * Increments the license reference count for recording.
+ *
+ * @return True if the reference count was incremented successfully, false otherwise.
+ */
 bool LicRecordRef()
 {
 	return TRUE;
 }
+/**
+ * Decrements the license reference count for recording.
+ *
+ * @return True if the reference count was decremented successfully, false otherwise.
+ */
 bool LicRecordUnRef()
 {
 	return TRUE;
 }
+/**
+ * Increments the license reference count for playback.
+ *
+ * @return True if the reference count was incremented successfully, false otherwise.
+ */
 bool LicPlayRef()
 {
 	return TRUE;
 }
+/**
+ * Decrements the license reference count for playback.
+ *
+ * @return True if the reference count was decremented successfully, false otherwise.
+ */
 bool LicPlayUnRef()
 {
 	return TRUE;
--- velib/src/utility/license.hpp
+++ velib/src/utility/license.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018 Heimdall
+ * Copyright (c) 2017-2023 Heimdall
  *
  * The computer program contained herein contains proprietary
  * information which is the property of Heimdall.
@@ -15,13 +15,68 @@
 
 using Poco::Environment;
 
+/**
+ * Sets the license.
+ *
+ * @param lic The license to be set.
+ * @return True if the license was set successfully, false otherwise.
+ */
 BOOL SetIc(string lic);
+
+/**
+ * Checks if the license is valid.
+ *
+ * @return True if the license is valid, false otherwise.
+ */
 BOOL IsIced();
+
+/**
+ * Sets the license using the provided license string.
+ *
+ * @param strLicense A reference to the license string.
+ * @return True if the license was set successfully, false otherwise.
+ */
 BOOL LicSetLicense(astring &strLicense);
+
+/**
+ * Retrieves information about the license.
+ *
+ * @param strHostId A reference to the host ID.
+ * @param ch A reference to the license channel.
+ * @param type A reference to the license type.
+ * @param startTime A reference to the license start time.
+ * @param expireTime A reference to the license expiration time.
+ * @return True if the license information was retrieved successfully, false otherwise.
+ */
 BOOL LicGetLicenseInfo(astring &strHostId, int &ch, astring &type, 
-				astring &startTime, astring &expireTime);
+                      astring &startTime, astring &expireTime);
+
+/**
+ * Increments the license reference count for recording.
+ *
+ * @return True if the reference count was incremented successfully, false otherwise.
+ */
 bool LicRecordRef();
+
+/**
+ * Decrements the license reference count for recording.
+ *
+ * @return True if the reference count was decremented successfully, false otherwise.
+ */
 bool LicRecordUnRef();
+
+/**
+ * Increments the license reference count for playback.
+ *
+ * @return True if the reference count was incremented successfully, false otherwise.
+ */
 bool LicPlayRef();
+
+/**
+ * Decrements the license reference count for playback.
+ *
+ * @return True if the reference count was decremented successfully, false otherwise.
+ */
 bool LicPlayUnRef();
+
 #endif /* __V_LICENSE_HPP__ */
