683faa1ebb24b2a8f6e2465490b01faecb79b6df
raoufriahi <riahi.raouf@gmail.com>
2023-06-19 23:02:25 +0200

    documenataion for webserver

--- vecvr/main/webserver.cpp
+++ vecvr/main/webserver.cpp
@@ -2,9 +2,9 @@
  * Copyright (c) 2017-2023 Viva Technology
  *
  * The computer program contained herein contains proprietary
- * information which is the property of Heimdall.
+ * information which is the property of Viva Technology.
  * The program may be used and/or copied only with the written
- * permission of Heimdall or in accordance with the
+ * permission of Viva Technology or in accordance with the
  * terms and conditions stipulated in the agreement/contract under
  * which the programs have been supplied.
  */
