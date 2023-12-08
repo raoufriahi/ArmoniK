107c916cad50a5c31c73452d5b168635fccdea63
raoufriahi <riahi.raouf@gmail.com>
2023-12-08 15:09:55 +0100

    added new include

---
+++ vecvr/include/mediaserver.hpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2017-2023 Viva Technology
+ *
+ * The computer program contained herein contains proprietary
+ * information which is the property of Viva Technology.
+ * The program may be used and/or copied only with the written
+ * permission of Viva Technology or in accordance with the
+ * terms and conditions stipulated in the agreement/contract under
+ * which the programs have been supplied.
+ */
+#ifndef __VE_MEDIA_SERVER_H__
+#define __VE_MEDIA_SERVER_H__
+#include "server/factory.hpp"
+
+class VEMediaChannel;
+
+typedef std::map<string, VEMediaChannel*> VEMediaChannelMap;
+/**
+ * \class VEMediaServer
+ * \brief Represents a media server.
+ */
+class VEMediaServer 
+{
+public:
+	VEMediaServer(Factory &pFactory);
+	~VEMediaServer();
+	
+public:
+    /**
+	 * \brief Initializes the media server.
+	 * \return true if the initialization is successful, false otherwise.
+	 */
+	static bool InitMediaServer();
+	
+public:
+    /**
+	 * \brief Callback function for device change events.
+	 * \param pData Pointer to additional data.
+	 * \param change The FactoryCameraChangeData object representing the change event.
+	 * \return true if the callback was successfully executed, false otherwise.
+	 */
+	static bool DeviceChangeCallbackFunc(void* pData, FactoryCameraChangeData change);
+	
+	/**
+	 * \brief Callback function for device change events.
+	 * \param change The FactoryCameraChangeData object representing the change event.
+	 * \return true if the callback was successfully executed, false otherwise.
+	 */
+	bool DeviceChangeCallbackFunc1(FactoryCameraChangeData change);
+	
+public:
+    /**
+	 * \brief Initializes all devices.
+	 * \return true if the initialization is successful, false otherwise.
+	 */
+	bool InitDevices();
+
+	/**
+	 * \brief Initializes a single device.
+	 * \param pParam The VidCamera object representing the device parameters.
+	 * \return true if the initialization is successful, false otherwise.
+	 */
+	bool InitOneDevice(VidCamera  pParam);
+
+	/**
+	 * \brief Deletes a single device.
+	 * \param strId The ID of the device to delete.
+	 * \return true if the deletion is successful, false otherwise.
+	 */
+	bool DeleteOneDevice(astring strId);
+	
+private:
+	VEMediaChannelMap m_ChannelMap;   // A map of media channel names to VEMediaChannel pointers.
+	Factory &m_pFactory;              // A reference to the Factory object.
+};
+
+#endif /* __VE_MEDIA_SERVER_H__ */
