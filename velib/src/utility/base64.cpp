be21379aeb41e49b515fb87916d4988740890736
Raouf RIAHI <65498891+raoufriahi@users.noreply.github.com>
2024-01-08 12:04:41 +0000

    updated utility project

--- velib/src/utility/base64.cpp
+++ velib/src/utility/base64.cpp
@@ -1,29 +1,13 @@
-/* 
-   base64.cpp and base64.h
-
-   Copyright (C) 2004-2008 Renéyffenegger
-
-   This source code is provided 'as-is', without any express or implied
-   warranty. In no event will the author be held liable for any damages
-   arising from the use of this software.
-
-   Permission is granted to anyone to use this software for any purpose,
-   including commercial applications, and to alter it and redistribute it
-   freely, subject to the following restrictions:
-
-   1. The origin of this source code must not be misrepresented; you must not
-      claim that you wrote the original source code. If you use this source code
-      in a product, an acknowledgment in the product documentation would be
-      appreciated but is not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-      misrepresented as being the original source code.
-
-   3. This notice may not be removed or altered from any source distribution.
-
-   Renéyffenegger rene.nyffenegger@adp-gmbh.ch
-
-*/
+/*
+ * Copyright (c) 2017-2024 UbVideo
+ *
+ * The computer program contained herein contains proprietary
+ * information which is the property of UbVideo.
+ * The program may be used and/or copied only with the written
+ * permission of UbVideo or in accordance with the
+ * terms and conditions stipulated in the agreement/contract under
+ * which the programs have been supplied.
+ */
 
 #include "base64.hpp"
 #include <iostream>
