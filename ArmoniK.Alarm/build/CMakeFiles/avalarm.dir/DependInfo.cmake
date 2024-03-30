729c613f6795770d95e135a3cf41daa96428ceb7
Raouf RIAHI <65498891+raoufriahi@users.noreply.github.com>
2024-03-30 12:06:55 +0000

    Pulled and updated project sources from main branch

---
+++ ArmoniK.Alarm/build/CMakeFiles/avalarm.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/ArmoniK/avalarm/src/vscevent.cpp" "/workspaces/ArmoniK/avalarm/build/CMakeFiles/avalarm.dir/src/vscevent.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINUX_OS=1"
+  "avalarm_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../.."
+  ".././include"
+  "../../include"
+  "../../xcmnlib/include"
+  "../../3rdparty/leveldb/include"
+  "../../3rdparty/spdlog/include"
+  "../../3rdparty/protobuf/src"
+  "../../3rdparty/poco/Data/include"
+  "../../3rdparty/poco/Foundation/include"
+  "../../3rdparty/poco/Util/include"
+  "../../3rdparty/poco/Data/SQLite/include"
+  "../../velib/include"
+  "../../engine/include"
+  "../../avconfig/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
